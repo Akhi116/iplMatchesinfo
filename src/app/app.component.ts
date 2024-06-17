c04c5c28228547cc6baf42ac015a1ac8ec70e037
Akhil Kumar Sadhanala <akhil.sadhanala@fedex.com>
2024-06-17 14:28:46 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    TableComponent,
-    FormComponent,
-    PieChartComponent,
-    HttpClient,
-  ],
+  imports: [RouterOutlet, TableComponent, FormComponent, PieChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
